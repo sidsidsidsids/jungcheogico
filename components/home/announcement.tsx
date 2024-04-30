9a8232cf14ceb06c49c2a118c9ef06a53421da4d
sid <sid98816@gmail.com>
2024-04-30 19:39:40 +0900

    Design: text re-size

--- components/home/announcement.tsx
+++ components/home/announcement.tsx
@@ -1,7 +1,7 @@
 export default function Announcement() {
   const text = "정보처리기사 코딩 문제 솔루션";
   return (
-    <div className="w-72 sm:w-full text-xl w-48 xs:text-2xl sm:text-3xl lg:text-5xl p-6 font-noto_sans_kr text-center">
+    <div className="w-72 sm:w-full text-2xl w-48 xs:text-3xl sm:text-4xl lg:text-5xl p-6 font-noto_sans_kr text-center">
       {text.split("").map((char, index) => {
         let fontWeight = "font-thin";
         if (["정", "처", "기", "코"].includes(char)) {
